876d16288b7996ef4cd735208a1aaacfad4d2d6f
hexinyu <hexinyv@gmail.com>
2023-11-24 14:31:42 +0800

    Update to RockyLinux 8.9

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@
 ARCH_X86="x86_64"
 ARCH_ARM="aarch64"
 OS_MAJOR_VER="8"
-OS_FULL_VER="8.8"
-ROOTFS_VER="Base-8.8-20230518.0"
+OS_FULL_VER="8.9"
+ROOTFS_VER="Base-8.8-20231119.0"
 
 ROOTFS_FN_X86="Rocky-${OS_MAJOR_VER}-GenericCloud-${ROOTFS_VER}.${ARCH_X86}.qcow2"
 ROOTFS_URL_X86="https://dl.rockylinux.org/pub/rocky/${OS_FULL_VER}/images/${ARCH_X86}/${ROOTFS_FN_X86}"
